f4ea96f3374b422171b997c45b046ce394891bb8
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-02-14 14:59:17 +0530

    Initial commit.

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import flask
+
+app = flask.Flask( __name__ )
+
+@app.route('/', methods=['GET'])
+def sayHello():
+    return "Hello Python Microservice !"
+
+app.run(host='0.0.0.0',port=80)
